4647c8cb7e39b429f7912631e66ab798203ca482
이승우 <swoolee97@ajou.ac.kr>
2024-06-03 23:14:13 +0900

    백준 1024

---
+++ src/main/java/Main1024.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main1024 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        // 15 3 => 4 5 6
+        // 15 5 => 1 2 3 4 5
+
+    }
+}
